8da9154a7a701c3fbb7d7ca2c8fef546e2c366e3
Avinash <avinashyadav3276@gmail.com>
2024-08-25 18:22:00 +0530

    Arraysort

---
+++ SortingArray.java
@@ -0,0 +1,19 @@
+package Day1;
+
+import java.util.Arrays;
+
+public class SortingArray {
+
+	public static void main(String[] args) {
+
+		int num [] = {6,3,5,1,2,7,8,0,3,2,6};
+		
+	//	int sort = 0;
+	//	for (int i=0; i<num.length; i++) {
+			
+			Arrays.sort(num);
+		System.out.println(Arrays.toString(num));
+		
+	}
+
+}
---
+++ StringsortInArrays.java
@@ -0,0 +1,16 @@
+package Day1;
+
+import java.util.Arrays;
+
+public class StringsortInArrays {
+
+	public static void main(String[] args) {
+
+		String s []  = {"mumbai" , "pune", "lucknow", "Delhi"};
+		
+		Arrays.sort(s);
+		System.out.println("sorted string array is:" + Arrays.toString(s));
+		
+	}
+
+}
