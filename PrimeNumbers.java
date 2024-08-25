4072ec8f006d87695709ae7c5472a58de4034bf0
Avinash <avinashyadav3276@gmail.com>
2024-08-25 19:36:37 +0530

    PrimeNumber

---
+++ PrimeNumbers.java
@@ -0,0 +1,26 @@
+package ProgramsOnNumber;
+
+public class PrimeNumbers {
+
+	public static void main(String[] args) {
+
+		int num= 5;
+		int count = 0;
+		if(num>1) {
+			for(int i=1; i<=num; i++) {
+				if(num%i==0) 
+					count++;
+
+				}
+					if(count==2) {
+						System.out.println("number is prime number");
+					}else {
+						System.out.println("num is not prime number");
+					}
+		}
+				else {
+			System.out.println("num is not a prime number");
+		}
+	}
+
+}
