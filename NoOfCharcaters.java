073882ffa4ccba72e4f2cb4f02c74d028bb82502
Avinash <avinashyadav3276@gmail.com>
2024-08-27 11:19:10 +0530

    No of Characters

---
+++ NoOfCharcaters.java
@@ -0,0 +1,18 @@
+package Day7;
+
+public class NoOfCharcaters {
+
+	public static void main(String[] args) {
+
+		String s = "Avinash";
+		System.out.println(s.length());
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
