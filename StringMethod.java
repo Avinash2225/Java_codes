54d7ad7efb4c71bc237d856209cb23a2833eae6e
Avinash <avinashyadav3276@gmail.com>
2024-08-30 23:58:51 +0530

    StringMethods

---
+++ StringMethod.java
@@ -0,0 +1,53 @@
+package Day1;
+
+public class StringMethod {
+
+	public static void main(String[] args) {
+
+//String S = "welcome";
+//String s = new String("welcome");
+//System.out.println(s);
+
+	//}
+
+//}
+
+//String G = "welcome to hills side";
+//String g = new String("welcome to hills side");
+//System.out.println(G);
+//}
+//}
+
+//  String K = "welcome to the deoria";
+// String d = new String ("welcome to the deoria");
+//  System.out.println(d);
+//  }
+//  }
+
+//String J = "Welcome to the Lucknow";
+//String j = new String ("Welcome to the lucknow");
+//System.out.println(J);//
+//}
+//}
+
+//String s = "where is the shimla";
+//System.out.println(s);
+//}
+//}
+
+String p = "welcome to the jammu";
+System.out.println(p);
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
