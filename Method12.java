256285db0cd82003329e394a27285e00a416ea3b
Avinash <avinashyadav3276@gmail.com>
2024-08-31 22:27:27 +0530

    MethodCall

---
+++ Method12.java
@@ -0,0 +1,37 @@
+package day1;
+
+public class Method12 {
+
+static void Add() {
+	System.out.println("This is  1");
+}
+
+	
+
+static void Add2(int a) {
+	System.out.println("This is  2");
+
+}
+
+
+
+static void Add3(int b) {
+	System.out.println("This is  3");
+
+}
+	
+	public static void main(String[] args) {
+
+//	Method12 v1 = new Method12();
+//v1.Add();
+//v1.Add2(12);
+//v1.Add3(90);
+	Add();
+	Add2(7);
+	Add3(6);
+	
+	}
+
+}
+
+
