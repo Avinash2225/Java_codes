5c8e411ebae86021f47590b9d172dbad6d1c2949
Avinash <avinashyadav3276@gmail.com>
2024-09-04 16:57:46 +0530

    ConcatnationOfTheString

---
+++ ConcatOfString.java
@@ -0,0 +1,37 @@
+package Day2;
+
+public class ConcatOfString {
+
+//	Question 1 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	//	String S1= " Italy ";
+//		String S2 = " Germany ";
+//		String S3 = " Norway ";
+	//	System.out.println(S1 + S2 + S3);
+//System.out.println(S1.concat(S2 +S3));
+	
+		// Question 2
+	//	String S1 = " Delhi";
+	//	String S2 = " Mumbai";
+	//	String S3 = " Lucknow";
+	//	System.out.println(S1.concat(S2+S3));
+//		System.out.println("Delhi" + " Mumbai" + " lucknow");
+//		}
+//}
+// Question 3
+		
+/*String S4 = " Science";
+String S5 = " Social Science";
+String S6 = " Moral science";
+System.out.println(S4+S5+S6);
+}
+}*/
+		
+	String s1 = "Avinash";
+	String s2 = " Yadav";
+System.out.println("concation of the string is:" + s1 +s2);
+	
+	}
+}
+		
