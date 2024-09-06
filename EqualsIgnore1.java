fa9c5dd2045c29eeb6f016f64576a6499d162db3
Avinash <avinashyadav3276@gmail.com>
2024-09-06 16:22:15 +0530

    String Manupulations

---
+++ EqualsIgnore1.java
@@ -0,0 +1,20 @@
+package Day9;
+
+public class EqualsIgnore1 {
+
+	public static void main(String[] args) {
+
+		String s1 = "avinash";
+		String s2 = "Avinash";
+		
+		System.out.println(s1.equalsIgnoreCase(s2));
+		
+		System.out.println(s1.equals(s2));
+		
+		
+		
+		
+		
+	}
+
+}
