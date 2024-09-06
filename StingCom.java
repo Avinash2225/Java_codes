8119d06f60d5b87d23af35e6455e5ebe791fa308
Avinash <avinashyadav3276@gmail.com>
2024-09-06 16:22:15 +0530

    String Manupulations

---
+++ StingCom.java
@@ -0,0 +1,20 @@
+package Day8;
+
+public class StingCom {
+
+	public static void main(String[] args) {
+
+		String s1 = "Avinash";
+		String s2 = "Avinash1";
+		
+		System.out.println(s1.equals(s2));
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
