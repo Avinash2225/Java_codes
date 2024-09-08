e60b698a11621de90edd4f26c29968fd76390c15
Avinash <avinashyadav3276@gmail.com>
2024-09-08 18:47:15 +0530

    Reverse and sum of the Array

---
+++ ReverseAnArray.java
@@ -0,0 +1,21 @@
+package ArrayQuestions;
+
+public class ReverseAnArray {
+
+	public static void main(String[] args) {
+
+		
+		int a [] = {1,2,3,4,5,6};
+		int sum =0;
+		for( int i=a.length-1; i>=0; i--) {
+	//	for(int i =0 ; i<a.length;  i++) { // logic for sum of the Array
+	System.out.println(a[i] + " ");// logic for reverse an array
+		
+	//sum = sum + a[i];
+		
+		}
+	//	System.out.println(sum);
+	
+	}
+
+}
