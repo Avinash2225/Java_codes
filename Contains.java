6a780d67913f7740b6c5c6c574e6cb11ce57df41
Avinash <avinashyadav3276@gmail.com>
2024-09-26 23:30:18 +0530

    Contains

---
+++ Contains.java
@@ -0,0 +1,24 @@
+package Day1;
+
+public class Contains {
+
+	public static void main(String[] args) {
+
+		String s1 = "Avinash";
+		String s2 = "avinash";
+		
+		s1.equalsIgnoreCase(s2);
+		
+	boolean b1=	s1.contains(s2);
+	System.out.println(b1);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
