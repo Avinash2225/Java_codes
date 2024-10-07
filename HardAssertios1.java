7835ed37acfc64c5dd031ff79b3baa78339135ec
Avinash <avinashyadav3276@gmail.com>
2024-10-07 23:19:35 +0530

    HardAnnotations

---
+++ HardAssertios1.java
@@ -0,0 +1,19 @@
+package assertionsintestng;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HardAssertios1 {
+	
+	@Test
+	void test() {
+		//Assert.assertEquals("abc", "abc");
+		//Assert.assertEquals(123, 345);
+	//	Assert.assertEquals("abc", 145);
+	Assert.assertEquals("adc", 344);	
+Assert.assertNotEquals(234, 456);
+	
+	}
+	
+
+}
