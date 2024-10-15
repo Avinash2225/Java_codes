dd51429828dca53f43b7ba9ea03f05d84f887c9b
Avinash <avinashyadav3276@gmail.com>
2024-10-15 12:42:03 +0530

    ArrayDecleration

---
+++ ArrayDeclerationAndInitialization.java
@@ -0,0 +1,18 @@
+package ArrayQuestions;
+
+public class ArrayDeclerationAndInitialization {
+
+	public static void main(String[] args) {
+
+		int a [] = new int [4];
+		a[0] = 1;
+		a[1] = 2;
+		a[2] = 3;
+		a[3]= 4;
+		
+	//	System.out.println(a[4]);
+		System.out.println(a[3]);
+
+	}
+
+}
