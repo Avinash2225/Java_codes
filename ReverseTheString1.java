bce64f45f251a74fc189645ce8c48550ec2e813b
Avinash <avinashyadav3276@gmail.com>
2024-10-20 22:01:56 +0530

    Reverse

---
+++ ReverseTheString1.java
@@ -0,0 +1,24 @@
+package Day1;
+
+public class ReverseTheString1 {
+
+	public static void main(String[] args) {
+
+		String s = "Selenium";
+		
+		String rev = "";
+		
+		for( int i= s.length()-1; i>=0; i-- ) {
+			
+			rev = rev + s.charAt(i);
+		}
+		 System.out.println(rev);
+		
+		
+		
+		
+		
+		
+	}
+
+}
