c2697e87a4a5e91eea3cce692b120288c5525abe
Avinash <avinashyadav3276@gmail.com>
2024-10-21 23:47:23 +0530

    StringMethodCharAt

---
+++ CharAt.java
@@ -0,0 +1,37 @@
+package Day2;
+
+public class CharAt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//String S1 = "welcome to the Pune";
+//System.out.println(S1.charAt(12));
+	//	}
+//}
+
+// Contains Method used most of the times when we did validation
+// Question 2
+		
+/*String S1 = "Welcome To the Mau";
+String S2 = "Repeation";
+System.out.println(S1.contains(S2));
+	}
+}*/
+// Question 3
+		
+/*		String S1= "here is the Mmangoes";
+		String S2 = "here is ";
+		System.out.println(S1.contains(S2)); 
+		String S = "WELCOME";
+		System.out.println(S.contains("REST"));
+		*/
+		
+		String s1 = "Avinash";
+		System.out.println(s1.charAt(6));   // this method will give the character at the given index and index starts from 0 and length starts from 1
+		
+		
+		
+	
+		
+	}
+}
