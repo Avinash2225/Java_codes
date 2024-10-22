d1da2115b5d3dff35e22081e64486013386ba3d6
Avinash <avinashyadav3276@gmail.com>
2024-10-22 23:41:26 +0530

    JavaScriptexecuter

---
+++ Euqals.java
@@ -0,0 +1,26 @@
+package Day2;
+
+public class Euqals {
+
+	public static void main(String[] args) {
+		// Question 1
+/*String S = "Welcome";
+String		S2= "Welcome";
+System.out.println(S==S2);
+	}
+
+}*/
+		// Question 2
+		/* String S1 = "Welcome";
+		String S2 = "WELCOME";
+		System.out.println(S1.equalsIgnoreCase(S2));
+	}
+} */
+    //     Question 3
+String S1 = "Welcome";
+String S2 = "WELCOME";
+System.out.println(S1.equalsIgnoreCase(S2));
+}
+}		
+
+
