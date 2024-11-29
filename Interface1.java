05a7458ef640706be0167bc5b3094d5f2b2e4880
Avinash <avinashyadav3278@gmail.com>
2024-11-29 23:21:48 +0530

    Interface

---
+++ Interface1.java
@@ -0,0 +1,41 @@
+package interfaces;
+
+public interface Interface1 {
+
+	void applytheorem(int a);
+	void applythrem(int b);
+
+}
+
+interface cycle{
+	void add(int c);
+	void sub(int d);
+	
+}
+class avoncycle implements Interface1, cycle{
+
+	@Override
+	public void add(int c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sub(int d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void applytheorem(int a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void applythrem(int b) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ Interface2.java
@@ -0,0 +1,22 @@
+package interfaces;
+
+public class Interface2 {
+
+	
+	
+	interface camera{
+		void takessnap();
+		void recordvideo();
+		
+	}
+	interface wifi{
+		String[] getnetwork();
+		void connecttonetwork();
+	}
+	class mycellphone{
+		void callNumber() {
+			
+		}
+		
+	}
+}
