7cd5be086bb1dd69c31a5824ab09655f2651c471
Avinash <avinashyadav3276@gmail.com>
2024-12-23 23:33:02 +0530

    Packages

---
+++ ClassTest1.java
@@ -0,0 +1,23 @@
+package package1;
+
+public class ClassTest1 {
+
+public static void Add() {
+	int a =4;
+}
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+
+	
+	
+		
+		
+	}
+
+}
---
+++ ClassTest2.java
@@ -0,0 +1,13 @@
+package package1;
+
+public class ClassTest2 {
+
+
+	
+	public static void main(String[] args) {
+
+		ClassTest1 c1 = new ClassTest1();
+		
+	}
+
+}
