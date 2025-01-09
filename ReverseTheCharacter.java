251b1bedc780277ea3f7934591b8cdf73655815d
Avinash <avinashyadav3276@gmail.com>
2025-01-09 23:20:28 +0530

    Reverseeachword

---
+++ ReverseTheCharacter.java
@@ -0,0 +1,27 @@
+package trickystringquestions;
+
+public class ReverseTheCharacter {
+
+	public static void main(String[] args) {
+  String s = "Today is Monday";
+  
+  String[] word = s.split("");
+  
+  String reverseString = "";
+  
+  for(String w:word) {
+	  String reverseword = "";
+for(int i=w.length()-1; i>=0; i--) {
+	reverseword = reverseword+w.charAt(i);
+}
+reverseString = reverseString + reverseword + " ";
+
+  }
+  
+
+System.out.println(reverseString);	
+		
+		
+	}
+
+}
