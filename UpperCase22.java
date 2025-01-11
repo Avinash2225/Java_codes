93c1e017628c9fc587d23740f834552dff16fbb6
Avinash <avinashyadav3276@gmail.com>
2025-01-11 23:33:15 +0530

    uppercase

---
+++ UpperCase22.java
@@ -0,0 +1,19 @@
+package Day7;
+
+public class UpperCase22 {
+
+	public static void main(String[] args) {
+
+		String s = "pune and mumbai";
+		
+		System.out.println(s.toUpperCase());
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
