5f605e9a7ac873214e5752f9f5d2a1b040288660
Avinash <avinashyadav3276@gmail.com>
2025-01-18 22:55:33 +0530

    Stringinbuiltmethod

---
+++ LowerCase24.java
@@ -0,0 +1,21 @@
+package Day7;
+
+public class LowerCase24 {
+
+	public static void main(String[] args) {
+
+		String s = "WE WENT TO JAISALMER";
+		
+		System.out.println(s.toLowerCase());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ UpperCase23.java
@@ -0,0 +1,13 @@
+package Day7;
+
+public class UpperCase23 {
+
+	public static void main(String[] args) {
+
+		String s = "we went to rajasthan trip";
+		
+		System.out.println(s.toUpperCase());
+			
+	}
+
+}
